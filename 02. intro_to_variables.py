e3d65bb303dbae51ceeddeba41882882dc43d52c
Muhaimen Ayyub <63911969+MuhaimenAyyub@users.noreply.github.com>
2024-08-20 15:08:01 +0500

    Add files via upload

---
+++ 02. intro_to_variables.py
@@ -0,0 +1,47 @@
+# Create 4 variables for traveling. flight_cost, insurance, passport, baggage.
+# Create a variable to calculate the cost of all of them
+# print off the total cost with strings and variables
+# NOTE -> There are 5 people traveling!
+
+flight_cost = 300
+insurance = 50
+passport = 100
+baggage = 20
+
+cost_of_all = (flight_cost + insurance + passport + baggage) * 5
+
+# print("Total Cost:" , cost_of_all)
+
+# create 4 variables for ticket price. 1 variable is for the number of adults & 1 for children
+# Create 1 variable for the adult price and 1 for the child price
+# Create a variable to calculate the total
+# print off the total with strings and variables
+
+adult = 2
+children = 2
+
+adult_price = 55
+children_price = 27
+
+total = (adult * adult_price) + (children * children_price)
+
+# print("No Way:" , total)
+
+# A resort offers the normal price of 500 per night, a family wants to stay for 1 week.
+# The resort is offering a special promo of 30% off
+# print off the last minute trip price with strings and variables
+
+price = 500
+days = 7
+total = (price * days) * 0.7
+# print("Special discount (Hurrah):" , total)
+
+# A resort offers the normal price of 500 per night, a family wants to stay for 1 week.
+# The resort is offering a special promo of 30% off
+# print off the last minute trip price with strings and variables
+
+price = 1000
+no_of_days = 29
+
+total = (price * no_of_days) * 0.7
+print("Wo Hoo!!:" , total)
